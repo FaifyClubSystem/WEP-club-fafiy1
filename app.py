1ef2b8b7fbc05b54eb0add6404373aee32ec52d8
FaifyClubSystem <faifafcdev@gmail.com>
2026-08-05 02:13:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = 'fifa_club_archiving_secret_key'
 # --- إعدادات الاتصال بـ Neon PostgreSQL ---
 NEON_DATABASE_URL = os.environ.get(
     'DATABASE_URL', 
-    'postgresql://neondb_owner:npg_6k3RhMmjqwZv@ep-tiny-unit-aubq8ke5-pooler.c-10.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    'postgresql://postgres:[Essa12121313$$$$]@db.ievhntskoenaqfcunwby.supabase.co:5432/postgres'
 )
 
 ADMIN_ROLES = [
